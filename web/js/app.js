1f1eb459b86df37573d1e2603f45afd3369f1945
Kenol David <kenoldavid.git@gmail.com>
2017-05-25 03:38:02 -0400

    Adding initial web assets, package.json and webpack.config.js

---
+++ web/js/app.js
@@ -0,0 +1,6 @@
+
+// import style from '../css/style.css';
+import style from '../scss/main.scss';
+
+// console.log('Your style.css is loaded.');
+console.info('Yahoo! Your main.scss is loaded.');
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './web/js/app.js',
+  output: {
+    filename: './web/bundles/app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [ 'css-loader', 'sass-loader' ]
+      })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('./web/css/style.css')
+  ]
+}
